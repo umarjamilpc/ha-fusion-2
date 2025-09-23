40f67f36cde661f2a77cb5ee356698b803ea2bbd
Umar Jamil <umar-jamil-pc@outlook.com>
2025-09-23 15:48:19 +0500

    Update Theme.svelte

--- src/lib/Components/Theme.svelte
+++ src/lib/Components/Theme.svelte
@@ -4,9 +4,7 @@
 
 	export let initial: any;
 
-	let css: string;
-
-	$: if (theme) update(theme);
+	let css;
 
 	/**
 	 * Map the theme entries into CSS variables
@@ -52,23 +50,9 @@
 			console.error(error);
 		}
 	}
-
-	function update(theme: any) {
-		css = `
-			/* button */
-			--theme-button-background-color-off: ${theme?.['button-background-color-off'] || '#414141'};
-			--theme-button-name-color-off: ${theme?.['button-name-color-off'] || 'white'};
-			--theme-button-state-color-off: ${theme?.['button-state-color-off'] || '#a7a7a7'};
-			--theme-button-background-color-on: ${theme?.['button-background-color-on'] || '#f2f2f2'};
-			--theme-button-name-color-on: ${theme?.['button-name-color-on'] || 'black'};
-			--theme-button-state-color-on: ${theme?.['button-state-color-on'] || '#5c5c5c'};
-
-			/* history */
-			--theme-history-color: ${theme?.['history-color'] || '#323232'};
-		`;
-	}
 </script>
 
 <svelte:head>
-	{@html `<style>${css}</style>`}
+	{@html `<style>:root {${css}}</style>`}
+	<meta name="theme-color" content={initial?.theme?.['app-color']} />
 </svelte:head>
